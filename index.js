fc43a75001bd72a95e78c0d38607b624d1abecae
Anthony Mai <adonksmasher@gmail.com>
2018-12-12 00:23:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+
+function theBeatlesPlay(musicians,instruments) {
+  var a =[];
+  
+  for (var i = 0; i < musicians.length; i++) {
+    a.push(musicians[i]+" plays "+ instruments[i]);
+  
+  }
+  
+  return a
+}
+  
+   
+function johnLennonFacts(array){
+  var i = 0;
+  while (i < array.length){
+    array[i] = (array[i] + "!!!");
+    i++
+  }
+  return array;
+}
+
+function iLoveTheBeatles(number){
+  var array[]
+}
